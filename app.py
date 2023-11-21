961153c417585be8a81666a1d92fcc591428ea75
rawnjitale <rawnjit152@gmail.com>
2023-11-21 09:17:42 +0545

    Hello 1

--- app.py
+++ app.py
@@ -1,12 +1,67 @@
-from flask import Flask
+import os
+from flask import Flask, render_template, request, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+from sqlalchemy.sql import func
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+        'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
+# ...
 
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(100), nullable=False)
+    lastname = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+    age = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime(timezone=True),
+                           server_default=func.now())
+    bio = db.Column(db.Text)
+
+    def __repr__(self):
+        return f'<Student {self.firstname}>'
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    students=Student.query.all()
+    return render_template('index.html',students=students)
+# ...
 
+@app.route('/search/<int:student_id>/')
+def student(student_id):
+    student = Student.query.get_or_404(student_id)
+    return render_template('student.html', student=student)
+# ...
+@app.route('/test')
+def test():
+    return redirect("/hello")
+@app.route('/hello')
+def hello1():
+    return "<p>Hello, World from \
+                redirected page.!</p>"
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        email = request.form['email']
+        age = int(request.form['age'])
+        bio = request.form['bio']
+        student = Student(firstname=firstname,
+                          lastname=lastname,
+                          email=email,
+                          age=age,
+                          bio=bio)
+        db.session.add(student)
+        db.session.commit()
 
+        return redirect("/")
+
+    return render_template('create.html')   
 if __name__ == '__main__':
     app.run()
